3bcba11470a4f2dbd319f870676a75ae258d1a29
Marline John <marlinejohn@gmail.com>
2024-01-09 23:47:42 +0100

    Add an array list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world')
-
-let favoriteFood = "Biryani";
-document.write(favoriteFood)
+let pokemonList=[
+    {
+        name: 'Bulbasaur',
+        height: 1,
+        type: ['Monster', 'Grass']
+    }, 
+    {
+        name: 'Beedrill',
+        height: 1,
+        type: 'Bug'
+    }, 
+    {
+        name: 'Electrode',
+        height: 2,
+        type: 'Mineral'
+    }];
