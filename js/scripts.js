950019c41aecd8985125f20ac9d56c39c2c0d68b
Marline John <marlinejohn@gmail.com>
2024-01-11 15:23:26 +0100

    Add a for loop with conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 let pokemonList=[
     {
         name: 'Bulbasaur',
-        height: 1,
+        height: 2.04,
         type: ['Monster', 'Grass']
     }, 
     {
         name: 'Beedrill',
-        height: 1,
+        height: 3.03,
         type: ['Bug']
     }, 
     {
         name: 'Electrode',
-        height: 2,
-        type: ['Mineral']
+        height: 3.11,
+        type: ['Electric']
     }];
+
+    // Display names and height from the pokemonList.
+
+    for(let i = 0; i < pokemonList.length; i++ ){
+    // Display extra note if height of a character is greater than 3.1
+        if (pokemonList[i].height > 3.1){
+            document.write('<p>', pokemonList[i].name + ': ' + pokemonList[i].height + ' .Wow, that\'s big !! </p>');
+        } else{
+            document.write('<p>', pokemonList[i].name + ': ' + pokemonList[i].height  + ' </p>');
+
+        }    
+    }
+
+    
