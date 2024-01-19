671fd4ac06ada2e438220dd6a56489b3d87bdabe
Marline John <marlinejohn@gmail.com>
2024-01-19 01:18:57 +0100

    Function added to create and append elements

--- js/scripts.js
+++ js/scripts.js
@@ -23,11 +23,24 @@ let pokemonList=[
     function add(item) {
         pokemonList.push(item);
     }
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let li = document.createElement('li');
+        pokemonList.appendChild(li);
+    
+        let button = document.createElement('button');
+        button.innerHTML = pokemon.name;
+        li.appendChild(button);
+        button.classList.add('name-btn')
+    }
 
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     };
+
+    
 
 }) ();
 console.log( pokemonRepository.getAll());
@@ -36,11 +49,14 @@ console.log(pokemonRepository.add({ name: "Golem", height: 1.4, type: ['mineral'
 
 
 pokemonRepository.getAll().forEach(function(pokemon){
-        if (pokemon.height > 3.1){
-            document.write( '<p>', pokemon.name + ': ' + pokemon.height + ' .Wow, that\'s big !! </p>');
-        } else{
-            document.write('<p>', pokemon.name + ': ' + pokemon.height  + ' </p>');
-        }    
+   
+
+        // if (pokemon.height > 3.1){
+        //     document.write( '<p>', pokemon.name + ': ' + pokemon.height + ' .Wow, that\'s big !! </p>');
+        // } else{
+        //     document.write('<p>', pokemon.name + ': ' + pokemon.height  + ' </p>');
+        // }  
+        pokemonRepository.addListItem(pokemon)  
     })
 
 
